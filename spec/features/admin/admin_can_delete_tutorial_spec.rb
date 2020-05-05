71828cb5d2c6be1eeb2e23ad094775cf7b349ab1
Ryan Camp <campryan@comcast.net>
2020-05-05 17:45:33 -0600

    Write test to check deletion of videos when tutorial deleted

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -17,4 +17,30 @@ feature "An admin can delete a tutorial" do
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
+
+  it "and it should also delete associated videos" do
+    admin = create(:admin)
+    tutorial1 = create(:tutorial)
+    tutorial2 = create(:tutorial)
+    create_list(:video, 3, tutorial: tutorial1)
+    create_list(:video, 3, tutorial: tutorial2)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/admin/dashboard"
+
+    expect(Video.where(tutorial_id: tutorial1.id).count).to eql(3)
+    expect(Video.where(tutorial_id: tutorial2.id).count).to eql(3)
+
+    within("#tutorial-#{tutorial1.id}") do
+      click_link "Delete"
+    end
+
+    expect(page).to have_no_css("#tutorial-#{tutorial1.id}")
+    expect(page).to have_css("#tutorial-#{tutorial2.id}")
+
+    expect(Video.where(tutorial_id: tutorial1.id).empty?).to eql(true)
+    expect(Video.where(tutorial_id: tutorial2.id).empty?).to eql(false)
+  end
+
 end
