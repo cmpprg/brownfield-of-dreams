13621a641d48b41eb0ec1e9358371151e88b2f28
Ryan Camp <campryan@comcast.net>
2020-05-06 20:01:07 -0600

    Add ability to gather repos for current user to show controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  def show; end
+  def show
+    @repos = gather_repo_objects if github_token_present?
+  end
 
   def new
     @user = User.new
@@ -20,5 +22,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
+
+  def gather_repo_objects
+    repos_info = GithubV3API.new(current_user.github_token).repo_info[0..4]
+    RepositoryCollection.new(repos_info).return_collection
   end
 end
