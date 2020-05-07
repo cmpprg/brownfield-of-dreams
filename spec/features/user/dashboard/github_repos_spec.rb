46e6569a64b320603dd1343d25e7c8ea0ea5ed5f
Ryan Camp <campryan@comcast.net>
2020-05-06 19:55:06 -0600

    Create test to check links for github repos on user dashboard

--- spec/features/user/dashboard/github_repos_spec.rb
+++ spec/features/user/dashboard/github_repos_spec.rb
@@ -2,23 +2,51 @@ require "rails_helper"
 
 RSpec.describe "As a default user", type: :feature do
   describe "When I am on the dashboard" do
+    before(:each) do
+      @user = create(:user, github_token: ENV["GITHUB_TEST_API_KEY"])
+    end
+
     it "I can see a section called 'github' and 5 linked repos listed." do
-      user = create(:user, github_token: "28adf5157c3594b994aca2efa1f17912ec9b7459")
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       visit "/dashboard"
-
       within(".github") do
         expect(page).to have_content("Github")
         expect(page).to have_css(".repo", count: 5)
       end
+    end
 
-      # User Story:
-      # ** As a logged in user **
-      # ** When I visit /dashboard **
-      # ** Then I should see a section for "Github" **
-      # And under that section I should see a list of 5 repositories with the name of each Repo linking to the repo on Github
+    it "I can click on one of the repo titles and be taken to that repo", :js do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit "/dashboard"
+      expected_href = find_by_id("#repo-4")['href']
+
+      within(".github") do
+        expect(page).to have_link("#repo-0")
+        expect(page).to have_link("#repo-1")
+        expect(page).to have_link("#repo-2")
+        expect(page).to have_link("#repo-3")
+        expect(page).to have_link("#repo-4")
+        click_link "#repo-4"
+      end
+      expect(current_url).to eql(expected_href)
+    end
+
+    xit "I see my repos and not another registered user" do
+      user1 = create(:user, github_token: ENV["GITHUB_TEST_API_KEY"])
+      user2 = create(:user, github_token: ENV["ALT_GITHUB_TEST_API_KEY"])
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+
+      # Ask project manager about testing this edge case
+    end
+
+    it "I can't see a github section if I don't have a github token" do
+      @user.github_token = nil
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit "/dashboard"
+
+      expect(page).to have_no_css(".github")
     end
   end
 end
