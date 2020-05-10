6cd018320b14babb64849df9a06278677b777061
Ryan Camp <campryan@comcast.net>
2020-05-10 11:51:26 -0600

    Remove test for validation of description for Tutorial

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Tutorial, type: :model do
 
   describe "Validations" do
     it { should validate_presence_of :title}
-    it { should validate_presence_of :description}
     it { should validate_presence_of :thumbnail}
   end
 
