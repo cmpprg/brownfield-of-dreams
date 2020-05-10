d548daf0fd923a0be7bbdf82d35ce4425141f4ad
Ryan Camp <campryan@comcast.net>
2020-05-10 08:57:45 -0600

    Write test to check admin can create a tutorial

---
+++ spec/features/admin/admin_can_create_a_new_tutorial_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe 'As an admin on the new tutorials page', type: :feature do
+  before(:each) do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+
+  it "I can create a tutorial by entering the info in a form" do
+    visit '/admin/tutorials/new'
+    
+    fill_in 'tutorial[title]', with: 'title of tutorial'
+    fill_in 'tutorial[description]', with: 'description of tutorial'
+    fill_in 'tutorial[thumbnail]', with: 'https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg'
+    click_button 'Save'
+
+    tutorial = Tutorial.last
+    require 'pry'; binding.pry
+    expect(current_path).to eql(tutorial_path(tutorial))
+
+
+    expect(tutorial.title).to eql('title of tutorial')
+    expect(tutorial.description).to eql('description of tutorial')
+    expect(tutorial.thumbnail).to eql('https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg')
+  end
+end
+
+# When I visit '/admin/tutorials/new'
+# And I fill in 'title' with a meaningful name
+# And I fill in 'description' with a some content
+# And I fill in 'thumbnail' with a valid YouTube thumbnail
+# And I click on 'Save'
+# Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
+# And I should see a flash message that says "Successfully created tutorial.
