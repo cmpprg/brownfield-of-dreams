9e50668439c0a06bfa3a4e42fd9af2c9b1f9b112
Ryan Camp <campryan@comcast.net>
2020-05-10 21:27:57 -0600

    Add conditional to check if visitor and refactor

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,23 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      tagged_tutorials = Tutorial.tagged_with(params[:tag])
-      @tutorials = tagged_tutorials.paginate(page: params[:page], per_page: 5)
-    else
-      @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-    end
+    params[:tag] ? tagged_tutorials_allowed : all_tutorials_allowed
+  end
+
+  private
+
+  def tagged_tutorials_allowed
+    tag_tutorials = Tutorial.tagged_with(params[:tag])if current_user
+    tag_tutorials = non_classroom_tutorials.tagged_with(params[:tag])if visitor?
+    @tutorials = tag_tutorials.paginate(page: params[:page], per_page: 5)
+  end
+
+  def all_tutorials_allowed
+    tutorials = Tutorial.all if current_user
+    tutorials = non_classroom_tutorials if visitor?
+    @tutorials = tutorials.paginate(page: params[:page], per_page: 5)
+  end
+
+  def non_classroom_tutorials
+    Tutorial.where(classroom: false)
   end
 end
