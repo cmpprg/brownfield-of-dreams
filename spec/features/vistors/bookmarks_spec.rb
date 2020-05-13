440fab9db43468dffd46743b48c4c3f496d80dcc
Jesse Gietzen <gietzenjesse@gmail.com>
2020-05-13 11:27:36 -0600

    revise test to reflect button

--- spec/features/vistors/bookmarks_spec.rb
+++ spec/features/vistors/bookmarks_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'as a visitor when i click a bookmark link', type: :feature do
+RSpec.describe 'as a visitor when i click a bookmark button', type: :feature do
   it 'i see a flash message that i am not logged in and stay on that page' do
     tutorial = create(:tutorial)
     video = create(:video, tutorial_id: tutorial.id)
     visit "/tutorials/#{tutorial.id}?video_id=#{video.id}"
-    click_link 'Bookmark'
+    click_button 'Bookmark'
 
     expect(current_path).to eql("/tutorials/#{tutorial.id}")
     expect(current_url).to include("/tutorials/#{tutorial.id}?video_id=#{video.id}")
