50f06862b9c7dd27d5ea1b611e0e78a4d1cbe846
Ryan Camp <campryan@comcast.net>
2020-05-13 11:28:08 -0600

    Sudo code test for email, and add test to check active status

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe 'vister can create an account', :js do
-  it ' visits the home page' do
-    email = 'jimbob@aol.com'
-    first_name = 'Jim'
-    last_name = 'Bob'
-    password = 'password'
-    password_confirmation = 'password'
+  before(:each) do
+    @email = 'jimbob@aol.com'
+    @first_name = 'Jim'
+    @last_name = 'Bob'
+    @password = 'password'
+    @password_confirmation = 'password'
+  end
 
+  it ' visits the home page' do
     visit '/'
 
     click_on 'Sign In'
@@ -18,11 +20,11 @@ describe 'vister can create an account', :js do
 
     expect(current_path).to eq(new_user_path)
 
-    fill_in 'user[email]', with: email
-    fill_in 'user[first_name]', with: first_name
-    fill_in 'user[last_name]', with: last_name
-    fill_in 'user[password]', with: password
-    fill_in 'user[password_confirmation]', with: password
+    fill_in 'user[email]', with: @email
+    fill_in 'user[first_name]', with: @first_name
+    fill_in 'user[last_name]', with: @last_name
+    fill_in 'user[password]', with: @password
+    fill_in 'user[password_confirmation]', with: @password
 
     click_on'Create Account'
 
@@ -31,9 +33,38 @@ describe 'vister can create an account', :js do
 
     expect(page).to have_content("Logged in as #{user.first_name}")
     expect(page).to have_content("This account has not yet been activated. Please check your email.")
-    expect(page).to have_content(email)
-    expect(page).to have_content(first_name)
-    expect(page).to have_content(last_name)
+    expect(page).to have_content(@email)
+    expect(page).to have_content(@first_name)
+    expect(page).to have_content(@last_name)
     expect(page).to_not have_content('Sign In')
   end
+
+  context "As a newly registered user" do
+    context "when I click link in my email 'Visit here to activate your account.'" do
+      it "I am take to a page that says 'Thank you! Your account is now activated.'" do
+        # allow_any_instance_of(AccountActivationMailer).to receive(:inform).and_return("I sent the thing!")
+        # visit new_user_path
+        # fill_in 'user[email]', with: @email
+        # fill_in 'user[first_name]', with: @first_name
+        # fill_in 'user[last_name]', with: @last_name
+        # fill_in 'user[password]', with: @password
+        # fill_in 'user[password_confirmation]', with: @password
+        #
+        # click_on'Create Account'
+
+        #unable to test properly, come back if there is time and figure this out.
+        # potential need to create class that captures email through ActionMailer, then a class to read?
+      end
+    end
+
+    it "after account activated display 'status: active' on the dashboard" do
+      user = create(:user, activation_status: 'active')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      expect(page).to have_content("Status: Active")
+    end
+  end
+
 end
