b6ab159aa6a893aed56ecab2beacb87d6992e8ec
Ryan Camp <campryan@comcast.net>
2020-05-12 18:05:22 -0600

    Refactor users controller and add flash messages for login and activate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,39 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to dashboard_path
-    else
-      flash[:error] = 'Username already exists'
-      render :new
-    end
+    @user = User.new(user_params)
+    @user.save ? create_happy_path(@user) : create_sad_path(@user)
   end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
+
+
+  def create_happy_path(user)
+    session[:user_id] = user.id
+    create_happy_flash(user)
+    please_activate_flash
+    redirect_to dashboard_path
+  end
+
+  def create_sad_path(user)
+    create_sad_flash(user)
+    render action: :new
+  end
+
+  def please_activate_flash
+    mess = "This account has not yet been activated. Please check your email."
+    flash[:activation_notice] = mess
+  end
+
+  def create_happy_flash(user)
+    flash[:notice] = "Logged in as #{user.first_name} #{user.last_name}"
+  end
+
+  def create_sad_flash(user)
+    flash[:error] = user.errors.full_messages.to_sentence
   end
 end
