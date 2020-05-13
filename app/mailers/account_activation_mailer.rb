4d1fbd5301a42cd2200c8a51ba22616c595f6523
Ryan Camp <campryan@comcast.net>
2020-05-12 22:13:22 -0600

    Create mailer for account activation

---
+++ app/mailers/account_activation_mailer.rb
@@ -0,0 +1,8 @@
+class AccountActivationMailer < ApplicationMailer
+  default from: 'jesse&ryan@example.com'
+  def inform(user)
+    @user = user
+    @message = 'Visit here to activate your account.'
+    mail(to: user.email, subject: 'click and activate')
+  end
+end
