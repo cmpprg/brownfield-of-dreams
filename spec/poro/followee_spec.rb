2ec35259d7339a84e41c5e487b340bcd91f0be04
Ryan Camp <campryan@comcast.net>
2020-05-13 15:28:20 -0600

    Add github_uid attribute to followee PORO spec

--- spec/poro/followee_spec.rb
+++ spec/poro/followee_spec.rb
@@ -2,16 +2,19 @@ require "rails_helper"
 
 RSpec.describe Followee, type: :model do
   before(:each) do
-    info = {login: "Followee Name", html_url: "http://www.example.com"}
+    info = {login: 'Followee Name',
+            html_url: 'http://www.example.com',
+            id: '12345678'}
     @followee = Followee.new(info)
   end
 
-  it "exists" do
+  it 'exists' do
     expect(@followee).to be_a(Followee)
   end
 
-  it "has_attributes" do
-    expect(@followee.name).to eql("Followee Name")
-    expect(@followee.url).to eql("http://www.example.com")
+  it 'has_attributes' do
+    expect(@followee.name).to eql('Followee Name')
+    expect(@followee.url).to eql('http://www.example.com')
+    expect(@followee.github_uid).to eql('12345678')
   end
 end
