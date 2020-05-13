07e1dd58225aa44157d97924053f551797265cff
Ryan Camp <campryan@comcast.net>
2020-05-13 15:28:50 -0600

    Add github_uid attribute to Followee

--- app/poros/followee.rb
+++ app/poros/followee.rb
@@ -1,8 +1,9 @@
 class Followee
-  attr_reader :name, :url
+  attr_reader :name, :url, :github_uid
 
   def initialize(followee_info)
     @name = followee_info[:login]
     @url = followee_info[:html_url]
+    @github_uid = followee_info[:id]
   end
 end
