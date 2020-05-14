1a81b0066784573f444439603c300e4bb34fbbd9
Ryan Camp <campryan@comcast.net>
2020-05-14 07:53:00 -0600

    production env, set perform deliveries for action mailer to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   Rails.application.routes.default_url_options[:host] = 'https://brownfield-jg-rc.herokuapp.com/'
+
+  config.action_mailer.perform_deliveries = false
 end
