66b62469bf62baedfaa48819761a3348c2da81dc
Ryan Camp <campryan@comcast.net>
2020-05-13 22:58:00 -0600

    Write test to check Friends displayed on dashboard

--- spec/features/user/dashboard/friendship_link_spec.rb
+++ spec/features/user/dashboard/friendship_link_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "As a user on the dashboard", type: :feature do
   before(:each) do
-    @user = create(:user, github_token: 'token', github_uid: '20000006')
+    @user = create(:user, github_token: 'token', github_uid: '20000006', github_handle: 'user_handle')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
     followers = File.read('spec/fixtures/github_follwers_friendship.json')
@@ -13,11 +13,11 @@ RSpec.describe "As a user on the dashboard", type: :feature do
       to_return(body: following)
 
     allow_any_instance_of(RepositoryFactory).to receive(:return_collection).and_return([])
-    @follower1 = create(:user, github_uid: '20000001')
-    @follower2 = create(:user, github_uid: '20000002')
-    @follower3 = create(:user, github_uid: '20000004')
-    @followee1 = create(:user, github_uid: '10000004')
-    @followee2 = create(:user, github_uid: '10000005')
+    @follower1 = create(:user, github_uid: '20000001', github_handle: "github_follower_user_1")
+    @follower2 = create(:user, github_uid: '20000002', github_handle: "github_follower_user_2")
+    @follower3 = create(:user, github_uid: '20000004', github_handle: "github_follower_user_4")
+    @followee1 = create(:user, github_uid: '10000004', github_handle: "github_following_user_4")
+    @followee2 = create(:user, github_uid: '10000005', github_handle: "github_following_user_5")
   end
 
   context 'If a follower or followee also has an account on this app' do
@@ -81,13 +81,31 @@ RSpec.describe "As a user on the dashboard", type: :feature do
         end
       end
 
+      expect(@user.friends.all).to include(@follower1)
+      expect(@user.friends.all).to include(@followee2)
+      expect(@user.friends.all).not_to include(@follower2)
+      expect(@user.friends.all).not_to include(@follower3)
+      expect(@user.friends.all).not_to include(@followee1)
+    end
 
+    it "I can see a list of friends I currently have" do
+      Friendship.create(user_id: @user.id, friend_id: @follower1.id)
+      Friendship.create(user_id: @user.id, friend_id: @followee2.id)
 
-      expect(@user.friends).to include(@follower1)
-      expect(@user.friends).to include(@followee2)
-      expect(@user.friends).not_to include(@follower2)
-      expect(@user.friends).not_to include(@follower3)
-      expect(@user.friends).not_to include(@followee1)
+      visit dashboard_path
+
+      within('.friendships') do
+        expect(page).to have_content("Friends")
+        within("#friend-#{@follower1.id}") do
+          expect(page).to have_content(@follower1.github_handle)
+        end
+        within("#friend-#{@followee2.id}") do
+          expect(page).to have_content(@followee2.github_handle)
+        end
+        expect(page).to have_no_content(@follower2.github_handle)
+        expect(page).to have_no_content(@follower3.github_handle)
+        expect(page).to have_no_content(@followee1.github_handle)
+      end
     end
   end
 end
